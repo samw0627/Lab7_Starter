730b442b4366c2c8b40f590d84a378aecd6eb29e
Sam Wong <97695769+samw0627@users.noreply.github.com>
2023-05-16 16:34:20 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -68,11 +68,15 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  var array = JSON.parse(localStorage.getItem("recipes"));
+  if (array != null){
+    return array;
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
-  // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
+   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
@@ -98,8 +102,31 @@ async function getRecipes() {
   //            if you have, then save the recipes to storage using the function
   //            we have provided. Then, pass the recipes array to the Promise's
   //            resolve() method.
+  var newarr = [];
+  return new Promise( async (resolve, reject)=> { 
+  for (var i = 0; i< RECIPE_URLS.length; i++){
+      try{
+        const res = await fetch(RECIPE_URLS[i]);
+        const data = await res.json();
+        newarr.push(data);
+        if(res.ok){
+          saveRecipesToStorage(newarr);
+          resolve(newarr);
+        }
+      }
+      catch (e){
+        console.error(e);
+        reject(e);
+      }
+  }
+  
+
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+  
+  }
+)
+ 
 }
 
 /**
